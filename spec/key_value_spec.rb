3d0f031d9a8d26faad24a012af88812e1c354f03
Alexey Astafyev <av.astafyev@gmail.com>
2012-05-19 10:53:46 +0400

    added rspec

---
+++ spec/key_value_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe Configus2 do
+  pending "write it"
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'configus2'
+
+RSpec.configure do |config|
+  # some (optional) config here
+end
