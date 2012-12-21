4dbf884ab886c8e3e592d6fdc7171fada4e8ce7c
Alexey Astafyev <av.astafyev@gmail.com>
2012-12-21 11:16:18 +0400

    embedded coveralls

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in configus2.gemspec
 gemspec
+
+gem 'coveralls', require: false
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'configus2'
+require 'coveralls'
+Coveralls.wear!
 
 RSpec.configure do |config|
   # some (optional) config here
